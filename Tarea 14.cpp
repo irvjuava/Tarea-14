e402b1ab91fd9e8c58f3300898ecd2addef54bb7
Irving Ricardo Juárez Vázquez <31520357+irvjuava@users.noreply.github.com>
2017-10-28 22:34:24 -0500

    Add files via upload

---
+++ Tarea 14.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX 5 
+
+int main() {
+	int edad=0;
+	const float PI=3.1416;
+	
+	printf("zcual es tu edad?");
+	scanf("%d",&edad);
+	
+	//MAX=6;
+	//PI=23.14159265;
+	
+	printf("tu edad es= %d \n", edad);
+	printf("Maximo= %d",MAX);
+	printf("valor de PI=%f",PI);
+	
+	return 0;
+}
